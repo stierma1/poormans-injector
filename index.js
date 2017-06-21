40ea01cdd2a54cad32231922bcadfb1b72e7e297
Mark Stier <stier020@Marks-MacBook-Pro-2.local>
2017-06-21 08:38:28 -0500

    Added resolvers

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var Injector = require("./lib/injector");
-var globalInjector = new Injector({});
+var globalInjector = new Injector({}, true);
 module.exports = {
   globalInjector: globalInjector,
   Injector:require("./lib/injector"),
-  Optional: require("./lib/optional")
+  Optional: require("./lib/optional"),
+  Resolvers: require("./lib/resolver")
 }
